ca9b29c871540c0ff926d29a4144f38bb62c674d
Leonardo <leonardo.a@acad.ifma.edu.br>
2023-03-24 20:26:39 -0300

    Login Sessions
    - Implemented Login sessions to users
    - Logout Session cleaning cookies
    - Login sessions have dynamic headers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "You have been signed in."
+    else
+      flash[:alert] = "Mensagem de alerta"
+      redirect_to sign_in_path
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "You have been signed out."
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new", as: :sign_up
   post "sign_up", to: "registrations#create"
+
   delete "logout", to: "sessions#destroy", as: :sign_out
+
+  get "sign_in", to: "sessions#new", as: :sign_in
+  post "sign_in", to: "sessions#create"
 
   root to: "main#index"
 end
