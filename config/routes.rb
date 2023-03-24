3abfe410daa38fbd74b11facbe6daa44c1f36b20
Leonardo <leonardo.a@acad.ifma.edu.br>
2023-03-24 18:00:46 -0300

    Updated Logout
    Logout Sessions
    Routes Updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new", as: :sign_up
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy", as: :sign_out
 
   root to: "main#index"
 end
